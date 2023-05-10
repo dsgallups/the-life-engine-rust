c2d8c94468cc4b8a8ff5685e414be373b8acd920
Daniel <dgallups@purdue.edu>
2023-05-10 18:32:30 -0400

    I made a box that tells me where my mouse position is...yay!

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ pub mod environment;
 pub mod organism;
 
 use bevy::prelude::*;
+use bevy::text::{BreakLineOn, Text2dBounds};
 use bevy::window::PrimaryWindow;
 use environment::WorldEnvironment;
 use organism::anatomy::Anatomy;
 use organism::cell::{Cell, CellType};
 use organism::Organism;
 use rand::prelude::*;
+
+const CELL_SIZE: f32 = 12.0;
 fn main() {
     println!("Hello, world!");
     App::new()
@@ -21,6 +24,7 @@ fn main() {
         .add_startup_system(spawn_camera)
         .add_startup_system(setup)
         .add_startup_system(spawn_first_organism)
+        .add_system(print_mouse_pos)
         .run();
 }
 
@@ -28,7 +32,7 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     let window = window_query.get_single().unwrap();
 
     commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 2.0),
         ..default()
     });
 }
@@ -54,38 +58,79 @@ pub fn setup(
         num_cols as f32
     };
 
-    let cell_size = window_minimum_size / grid_min_size;
-
-    let mut x = 0;
-    loop {
-        let mut y = 0;
-
-        loop {
+    let mut x: f32 = 0.0;
+    while x < window.width() {
+        let mut y: f32 = 0.0;
+        while y < window.height() {
             commands.spawn(SpriteBundle {
                 sprite: Sprite {
                     color: Color::rgb(random::<f32>(), random::<f32>(), random::<f32>()),
-                    custom_size: Some(Vec2::new(cell_size, cell_size)),
+                    custom_size: Some(Vec2::new(CELL_SIZE, CELL_SIZE)),
                     ..default()
                 },
-                transform: Transform::from_translation(Vec3::new(
-                    cell_size * x as f32,
-                    cell_size * y as f32,
-                    0.0,
-                )),
+                transform: Transform::from_translation(Vec3::new(x, y, 0.0)),
                 ..default()
             });
-
-            if y > num_rows {
-                break;
-            }
-            y += 1;
+            y += CELL_SIZE;
         }
-
-        if x > num_cols {
-            break;
-        }
-        x += 1;
+        x += CELL_SIZE;
     }
+
+    let font = asset_server.load("fonts/fira.ttf");
+    //create box for the mouse position
+    let text_style = TextStyle {
+        font,
+        color: Color::WHITE,
+        font_size: 22.0,
+    };
+    let box_size = Vec2::new(140.0, 30.0);
+    let box_position = Vec2::new(window.width() - (box_size.x / 2.0), box_size.y / 2.0);
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.20, 0.3, 0.70),
+                custom_size: Some(Vec2::new(box_size.x, box_size.y)),
+                ..default()
+            },
+            transform: Transform::from_translation(box_position.extend(0.0)),
+            ..default()
+        })
+        .with_children(|builder| {
+            builder.spawn((
+                Text2dBundle {
+                    text: Text {
+                        sections: vec![TextSection::new("(000.0, 000.0)", text_style.clone())],
+                        alignment: TextAlignment::Left,
+                        linebreak_behaviour: BreakLineOn::AnyCharacter,
+                    },
+                    text_2d_bounds: Text2dBounds {
+                        // Wrap text in the rectangle
+                        size: box_size,
+                    },
+                    // ensure the text is drawn on top of the box
+                    transform: Transform::from_translation(Vec3::Z),
+                    ..default()
+                },
+                MousePosBox,
+            ));
+        });
+    /*commands.spawn(Text2dBundle {
+        text: Text::from_section(
+            // Accepts a String or any type that converts into a String, such as &str.
+            "hello world!",
+            TextStyle {
+                font: font.clone(),
+                font_size: 60.0,
+                color: Color::WHITE,
+            },
+        ),
+        transform: Transform {
+            translation: Vec3::new(0.0, 0.0, 0.1),
+            ..default()
+        },
+        // ensure the text is drawn on top of the box
+        ..default()
+    });*/
 }
 
 pub fn spawn_first_organism(
@@ -116,4 +161,20 @@ pub fn spawn_first_organism(
     let window = window_query.get_single().unwrap();
     first_organism.abs_x = (window.width() / 2.0) as u64;
     first_organism.abs_y = (window.height() / 2.0) as u64;
+}
+
+#[derive(Component)]
+pub struct MousePosBox;
+
+pub fn print_mouse_pos(
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut mouse_pos_box: Query<&mut Text, With<MousePosBox>>,
+) {
+    let mut text = mouse_pos_box.get_single_mut().unwrap();
+    let window = windows.get_single().unwrap();
+    let pos = window.cursor_position();
+    match pos {
+        Some(pos) => text.sections[0].value = format!("({}, {})", pos.x, pos.y),
+        None => text.sections[0].value = format!("(n/a, n/a)"),
+    }
 }
