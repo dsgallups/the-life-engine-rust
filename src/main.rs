5500b02953812c1133b7c01f3a70e22639183c65
Daniel <dsgallups@protonmail.com>
2024-01-28 18:35:15 -0500

    move existing code to `old_src`

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-pub mod cell;
-//pub mod messages;
-pub mod organism;
-pub mod world;
-
-mod event;
-
-mod render;
-
-use render::begin_ticking;
-
-pub use cell::*;
-//pub use messages::*;
-pub use event::*;
-pub use organism::*;
-pub use world::*;
-
 fn main() {
-    begin_ticking();
+    println!("rewrite #3! this time, using ECS!!!");
 }
