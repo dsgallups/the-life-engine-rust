0cca5d51936e4a1d08390097152a8d067c0bcc09
dsgallups <dsgallups@protonmail.com>
2024-09-02 17:54:52 -0400

    refactor: genome reproduces

--- src/organism/mod.rs
+++ src/organism/mod.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 use genome::Genome;
-use rand::Rng as _;
 
 use crate::neighbor::VecExt;
 
@@ -26,12 +25,11 @@ pub struct Organism {
     can_move: bool,
     belly: u64,
     tick_born: u64,
-    mutation_rate: f64,
     offspring: u64,
 }
 
 impl Organism {
-    fn new(genome: Genome, belly: u64, tick_born: u64, mutation_rate: f64) -> Self {
+    fn new(genome: Genome, belly: u64, tick_born: u64) -> Self {
         let mut has_producer = false;
         let mut has_eye = false;
         let mut has_mover = false;
@@ -67,17 +65,12 @@ impl Organism {
             can_move: has_mover && !has_producer,
             belly,
             tick_born,
-            mutation_rate,
             offspring: 0,
         }
     }
     pub fn ready_to_reproduce(&self) -> bool {
         let can_reproduce_at = self.genome.num_cells() * 3;
         self.belly >= can_reproduce_at as u64
-    }
-
-    pub fn num_cells(&self) -> usize {
-        self.genome.num_cells()
     }
 
     /// returns the radius of self given its children
@@ -97,69 +90,19 @@ impl Organism {
         self.can_move
     }
 
-    pub fn genome(&self) -> &Genome {
-        &self.genome
-    }
-
-    /// loses half its food.
     pub fn reproduce(&mut self, current_tick: u64) -> Option<Organism> {
-        let mut rng = rand::thread_rng();
+        //always lose half of one's belly
         self.belly /= 2;
-
-        let mut child = self.clone();
-        if rng.gen::<bool>() {
-            child.mutation_rate += 1.
-        } else {
-            child.mutation_rate -= 1.
-        };
-
-        let mut mutation_list: Vec<MutationAction> = Vec::new();
-        loop {
-            let rng_val = rng.gen_range(0..=100);
-            if rng_val as f64 <= child.mutation_rate {
-                mutation_list.push(MutationAction::rand(&mut rng));
-            } else {
-                break;
-            }
-        }
-
-        for mutation in mutation_list {
-            match mutation {
-                MutationAction::New => self.genome.add_random_cell(&mut rng),
-                MutationAction::MutateOrgan => self.genome.mutate_random_cell(&mut rng),
-                MutationAction::Delete => {
-                    self.genome.delete_random_cell(&mut rng);
-                }
-            }
-        }
-
-        self.offspring += 1;
-
-        if child.num_cells() == 0 {
+        let child_genome = self.genome.reproduce();
+        if child_genome.num_cells() == 0 {
             return None;
         }
-        let mut has_mover = false;
-        let mut has_eye = false;
-        let mut has_producer = false;
-        for cell_type in child.genome().types() {
-            match cell_type {
-                OrganismCellType::Eye => has_eye = true,
-                OrganismCellType::Mover => has_mover = true,
-                OrganismCellType::Producer => has_producer = true,
-                _ => continue,
-            }
-
-            if has_mover && has_eye && has_producer {
-                break;
-            }
-        }
-
-        child.tick_born = current_tick;
-        Some(child)
+        self.offspring += 1;
+        Some(Self::new(child_genome, self.belly, current_tick))
     }
 
     pub fn first_organism() -> Self {
-        Self::new(Genome::first_organism(), 3, 0, 50.)
+        Self::new(Genome::first_organism(), 3, 0)
     }
     pub fn ate_food(&mut self, amt: u64) {
         self.belly += amt;
