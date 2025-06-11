cf5bb1ef269f342100caa4d44091ca8ca652bc96
Daniel Gallups <dsgallups@protonmail.com>
2025-06-11 10:47:02 -0400

    clean up default plugins

--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,21 @@ mod splash;
 mod theme;
 mod title;
 
-use bevy::{asset::AssetMetaCheck, prelude::*};
+use bevy::prelude::*;
 
 fn main() {
     let mut app = App::new();
     // Add Bevy plugins.
     app.add_plugins(
-        DefaultPlugins
-            .set(AssetPlugin {
-                // Wasm builds will check for meta files (that don't exist) if this isn't set.
-                // This causes errors and even panics on web build on itch.
-                // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-                meta_check: AssetMetaCheck::Never,
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Window {
+                title: "The Life Engine".to_string(),
+                fit_canvas_to_parent: true,
                 ..default()
-            })
-            .set(WindowPlugin {
-                primary_window: Window {
-                    title: "Template2d".to_string(),
-                    fit_canvas_to_parent: true,
-                    ..default()
-                }
-                .into(),
-                ..default()
-            }),
+            }
+            .into(),
+            ..default()
+        }),
     );
 
     app.add_plugins((loading::plugin, splash::plugin, title::plugin));
