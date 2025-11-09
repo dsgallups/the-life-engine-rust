b4b5177ea69b875cbb26a1556c60e5caeae18d0a
dsgallups <dsgallups@protonmail.com>
2025-11-09 08:44:44 -0500

    ideas bad

--- organism/src/network/topology.rs
+++ organism/src/network/topology.rs
@@ -1,13 +1,38 @@
+use bus::{Bus, BusReader};
+
 use crate::CellTemplate;
 
-pub struct NetworkTopology {}
+pub trait Junction {
+    /// The network's side that receives input
+    fn afference(&mut self) -> Option<Vec<JunctionAffer>>;
+    /// the network will push output here
+    fn efference(&mut self) -> Option<Vec<JunctionEffer>>;
+}
+
+/// This receives input
+pub struct JunctionAffer {
+    channel: BusReader<f32>,
+    cur_value: f32,
+}
+
+/// This receives input
+pub struct JunctionEffer {
+    channel: Bus<f32>,
+}
+
+#[derive(Default)]
+pub struct NetworkTopology {
+    inputs: Vec<JunctionAffer>,
+    outputs: Vec<JunctionEffer>,
+}
 
 impl NetworkTopology {
-    pub fn new_from_cells(cell_templates: Vec<CellTemplate>) -> Self {
-        for cell in cell_templates {
-            todo!()
+    pub fn insert_junction(&mut self, junction: &mut impl Junction) {
+        if let Some(afferers) = junction.afference() {
+            self.inputs.extend(afferers);
         }
-
-        todo!()
+        if let Some(efferers) = junction.efference() {
+            self.outputs.extend(efferers);
+        }
     }
 }
