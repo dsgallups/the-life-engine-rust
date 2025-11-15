6562225ba5b72bccb5e6557012012dfdc6aef4e1
dsgallups <dsgallups@protonmail.com>
2025-11-15 08:18:54 -0500

    going to build network specifically for organisms

--- nora/src/brain/mod.rs
+++ nora/src/brain/mod.rs
@@ -4,12 +4,16 @@ pub use state::*;
 mod scheduler;
 pub use scheduler::*;
 
+mod neuron;
+
 use rand::rngs::StdRng;
 
 use crate::prelude::JunctionAffer;
+#[derive(Default)]
 pub struct Brain {
-    name: String,
-    rng: StdRng,
+    // name: String,
+    // rng: StdRng,
+    state: BrainState,
     afference: Vec<JunctionAffer>,
 }
 
