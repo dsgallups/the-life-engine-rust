25df4c0170d96b9bd169f6769d812c5b1fcf35ca
dsgallups <dsgallups@protonmail.com>
2025-11-16 11:44:04 -0500

    move organism spawn code

--- organism/src/organism/mod.rs
+++ organism/src/organism/mod.rs
@@ -1,11 +1,7 @@
+mod spawn;
+pub use spawn::*;
+
 use crate::{
-    CellAssets,
-    CellKind,
-    CellOf,
-    Collagen,
-    DataCell,
-    Eye,
-    Launcher,
     genome::Genome, //old_genome::Genome,
 };
 use bevy::prelude::*;
@@ -23,80 +19,6 @@ impl Organism {
     }
 }
 
-#[derive(Message)]
-pub struct SpawnOrganism {
-    genome: Genome,
-    location: Vec2,
-}
-impl SpawnOrganism {
-    /// The receiving system will create offspring from this genome.
-    pub fn new(genome: Genome, location: Vec2) -> Self {
-        Self { genome, location }
-    }
-}
-
 pub fn plugin(app: &mut App) {
-    app.add_message::<SpawnOrganism>();
-    app.add_systems(Update, spawn_genomes);
-}
-
-fn spawn_genomes(
-    mut msgs: MessageReader<SpawnOrganism>,
-    mut commands: Commands,
-    assets: Res<CellAssets>,
-) {
-    for msg in msgs.read() {
-        let mut organism_genome = msg.genome.deep_clone();
-        organism_genome.scramble(&mut rand::rng());
-        let organism = commands
-            .spawn((
-                Name::new("Organism"),
-                Organism::new(msg.genome.clone()),
-                InheritedVisibility::VISIBLE,
-                Pickable::default(),
-                Transform::from_xyz(msg.location.x, msg.location.y, 0.),
-            ))
-            .id();
-
-        for (location, cell) in msg.genome.cells().map() {
-            let mut commands = commands.spawn((
-                cell.kind,
-                ChildOf(organism),
-                CellOf(organism),
-                Pickable::default(),
-                Transform::from_xyz(location.x as f32, location.y as f32, 0.),
-                Mesh2d(assets.cell.clone()),
-            ));
-            match cell.kind {
-                CellKind::Collagen => {
-                    commands.insert((
-                        Name::new("Collagen"),
-                        Collagen::default(),
-                        MeshMaterial2d(assets.white.clone()),
-                    ));
-                }
-                CellKind::Data => {
-                    commands.insert((
-                        Name::new("Data Cell"),
-                        DataCell::default(),
-                        MeshMaterial2d(assets.yellow.clone()),
-                    ));
-                }
-                CellKind::Launcher => {
-                    commands.insert((
-                        Name::new("Launcher Cell"),
-                        Launcher::default(),
-                        MeshMaterial2d(assets.red.clone()),
-                    ));
-                }
-                CellKind::Eye => {
-                    commands.insert((
-                        Name::new("Eye Cell"),
-                        Eye::default(),
-                        MeshMaterial2d(assets.sky.clone()),
-                    ));
-                }
-            }
-        }
-    }
+    app.add_plugins(spawn::plugin);
 }
